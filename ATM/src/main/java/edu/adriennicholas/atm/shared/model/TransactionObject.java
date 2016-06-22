20b18bb6169fa5c2c69ca82af77b60312df01f73
oneshizzle <oneshotnyc@msn.com>
2016-06-21 22:31:47 -0400

    removing auto added text.

--- ATM/src/main/java/edu/adriennicholas/atm/shared/model/TransactionObject.java
+++ ATM/src/main/java/edu/adriennicholas/atm/shared/model/TransactionObject.java
@@ -4,10 +4,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class TransactionObject implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 	
 	String type;
 	String id;
